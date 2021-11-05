5ee371644a062f3551f18471f9bb076c7626b279
DanielRGilles <danryangilles@gmail.com>
2021-11-05 10:22:23 -0700

    3 passing tests with different method types

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const getMethod = (request) => {
-    const method;
-    return method;
-}
+const getMethod = (Rawrequest) => {
+  const parseRequest = Rawrequest.split('\r');
+  console.log(parseRequest);
+  const [method, path] = parseRequest[0].split(' ') ;
+  console.log(method, path);
+  return method;
+};
 module.exports = getMethod;
--- index.test.js
+++ index.test.js
@@ -1,12 +1,27 @@
-const getMethod = require('./index')
+const getMethod = require('./index');
 describe('test exponent function', () => {
-    it('returns the method from the raw request', () => {
-      const rawRequest = `
-      GET / HTTP/1.1
-      Host: example.com
-      Accept-Language: us-en`;
-      const method = getMethod(rawRequest);
+  it('returns the method from the raw request', () => {
+    const rawRequest = `GET / HTTP/1.1\r
+Host: example.com\r
+Accept-Language: us-en\r`;
+    const method = getMethod(rawRequest);
 
-      expect(method).toEqual('GET');
-    });
+    expect(method).toEqual('GET');
+  });
+  it('returns the method from the raw request', () => {
+    const rawRequest = `POST / HTTP/1.1\r
+Host: example.com\r
+Accept-Language: us-en\r`;
+    const method = getMethod(rawRequest);
+
+    expect(method).toEqual('POST');
+  });
+  it('returns the method from the raw request', () => {
+    const rawRequest = `DELETE / HTTP/1.1\r
+Host: example.com\r
+Accept-Language: us-en\r`;
+    const method = getMethod(rawRequest);
+
+    expect(method).toEqual('DELETE');
+  });
 });
